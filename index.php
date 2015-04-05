3abcc020ce387e1b8a7414c4ccebe43c8087a31b
linroex <linroex@coder.tw>
2015-04-05 12:02:16 +0800

    change blade version to 4.*

--- index.php
+++ index.php
@@ -7,18 +7,27 @@
  * Author: linroex
  * Author URI: http://me.coder.tw
  */
+use Windwalker\Renderer\BladeRenderer;
+
+include(__DIR__ . '/vendor/autoload.php');
 
 class Selected_Post extends WP_Widget {
+    private $blade;
+
     public function __construct() {
         parent::__construct('selected_post', '精選文章集', ['description'=>'讓你可以挑選文章並顯示在側邊欄的小工具']);
+
+        $this->blade = new BladeRenderer(__DIR__ . '/templates', ['cache_path'=> __DIR__ . '/cache']);
+
     }
 
     public function widget($args, $instance) {
-
+        
     }
 
     public function form($instance) {
-
+        var_dump($this->blade->render('example', ['name'=>'linroex']));
+        exit();
     }
 
     public function update($new, $old) {
